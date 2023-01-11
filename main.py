b275d03bebbb9ddb041a11d47f3b497db51da9b0
dabdya <iamdabdya@gmail.com>
2023-01-11 19:57:43 +0500

    cors zaebal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from aiohttp import web
+import aiohttp_cors
+
 import argparse, ydb, ydb.iam 
 from functools import partial
 from datetime import datetime
@@ -101,6 +103,17 @@ if __name__ == "__main__":
     app.cleanup_ctx.append(
         partial(setup_database, args = parse_args())
     )
+
+    cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*"
+        )
+    })
+
+    for route in list(app.router.routes()):
+        cors.add(route)
 
     web.run_app(app)
     
